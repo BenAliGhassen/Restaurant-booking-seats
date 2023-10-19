fba09ab760cb3d74ffc47205b1a29c208f090a20
BenAliGhassen <ghassenbenali2@gmail.com>
2023-10-19 01:13:57 +0100

    Working on responsivness

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import logo from "./icons_assets/Logo .svg"
+import hmenu from "./icons_assets/hmenu.png"
 export const Nav = ()=>{
     return(
         <nav className='Nav_bar'>
             <section className='menu'>
         <img src={logo} alt='logo' id="logo"/>
         
-        <ul>
+        <ul id="bar_nav">
           <li className='Nav_menu'><a  href="#none">Home </a></li>
           <li className='Nav_menu'><a  href="#none">About</a></li>
           <li className='Nav_menu'><a  href="#none" >Menu</a></li>
@@ -13,6 +14,7 @@ export const Nav = ()=>{
           <li className='Nav_menu'><a  href="#none">Order online</a></li>
           <li className='Nav_menu'><a  href="#none">Login</a></li>
         </ul>
+        <img src={hmenu} alt="hamburger_menu" id="hamburger_menu"/>
         </section>
       </nav>
     );
